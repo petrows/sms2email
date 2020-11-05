fd1dd7323da65fab70dd3a615b8c8f5cfdc56f9a
Peter Petrovich <petrows@users.noreply.github.com>
2020-11-05 23:23:42 +0100

    Revert "Fix failed tests"

--- app/src/androidTest/java/ws/petro/sms2email/MatcherTest.kt
+++ app/src/androidTest/java/ws/petro/sms2email/MatcherTest.kt
@@ -1,11 +1,7 @@
 package ws.petro.sms2email
 
-import androidx.test.espresso.base.MainThread
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 import org.junit.Test
@@ -23,36 +19,32 @@ class MatcherTest {
     val matcher = Matcher(appContext)
 
     @Test
-    @MainThread
     fun matchRuleOrder() {
-        GlobalScope.launch {
+        runBlocking {
             ruleDao.deleteAll()
             ruleDao.insert(Rule("test-1", 3, -1, "test1@example.com"))
             ruleDao.insert(Rule("test-2", 2, -1, "test2@example.com"))
             ruleDao.insert(Rule("test-3", 1, -1, "test3@example.com"))
+            val matches = matcher.match("John Doe", "Hello world", 1)
 
-            matcher.match("John Doe", "Hello world", 1) { rules ->
-                assertTrue(rules.isNotEmpty())
-                assertEquals(rules[0].title, "test-3")
-            }
+            assertTrue(matches.isNotEmpty())
+            assertEquals(matches[0].title, "test-3")
         }
     }
 
     @Test
-    @MainThread
     fun matchRuleSim() {
-        GlobalScope.launch {
+        runBlocking {
             ruleDao.deleteAll()
             ruleDao.insert(Rule("test-1", 3, 3, "test1@example.com"))
             ruleDao.insert(Rule("test-2", 2, 2, "test2@example.com"))
             ruleDao.insert(Rule("test-3", 1, -1, "test3@example.com"))
 
-            matcher.match("John Doe", "Hello world", 2) { rules ->
-                assertFalse(rules.isNotEmpty())
-                assertEquals(rules[0].title, "test-3")
-                assertEquals(rules[1].title, "test-2")
-            }
-        }
+            val matches = matcher.match("John Doe", "Hello world", 2)
 
+            assertTrue(matches.isNotEmpty())
+            assertEquals(matches[0].title, "test-3")
+            assertEquals(matches[1].title, "test-2")
+        }
     }
 }
